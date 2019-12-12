93d8301a1a1ed2365e18f811fdd8a6e1c71c9c09
Oli Wennell <oli.wennell@gmail.com>
2019-12-12 17:13:52 +0000

    :tada: Initial framework

---
+++ day1/rocket-fuel.py
@@ -0,0 +1,8 @@
+import input
+
+def calculate_total_fuel(module_masses):
+    return 0
+
+totalFuelRequired = calculate_total_fuel(input.module_asses)
+
+print("Total fuel required is: " + str(totalFuelRequired))
